5e273cd9ebaa21c614215ec96c2bb58e3b0e0dad
mikgit <mikelhfzy@gmail.com>
2023-07-19 23:24:10 +0300

    michael

--- front/src/components/Admin/students/Students.jsx
+++ front/src/components/Admin/students/Students.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import './profilestatus.css'
-// import axios from 'axios'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 
 
@@ -10,18 +10,18 @@ const ProfileStatus = () => {
   const [user, setUser] = React.useState({})
 
 
-  // axios.defaults.withCredentials = true
-  // useEffect(() => {
-  //   axios.get('http://localhost:5000/student/studentdetails', { withCredentials: true })
-  //     .then((res) => {
-  //       setUser(res.data)
-  //     }).catch((error) => {
-  //       console.log(error.response.data.user)
-  //       if (error.response.data.user === false) {
-  //         navigate('/login')
-  //       }
-  //     })
-  // }, [])
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/student/allstudentMaterial')
+      .then((res) => {
+        setUser(res.data)
+      }).catch((error) => {
+        console.log(error)
+        // if (error.response.data.user === false) {
+        //   navigate('/login')
+        // }
+      })
+  }, [])
   return (
     <div className='container'>
     <h2>اضافه طالب</h2>
